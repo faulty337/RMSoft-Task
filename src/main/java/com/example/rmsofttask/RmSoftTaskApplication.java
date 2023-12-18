683d63a791d0be6a41f3349e79dac8a97f469bc5
faulty337 <faulty337@gmail.com>
2023-12-18 23:31:41 +0900

    Feat : start project

---
+++ src/main/java/com/example/rmsofttask/RmSoftTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.rmsofttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmSoftTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmSoftTaskApplication.class, args);
+	}
+
+}
