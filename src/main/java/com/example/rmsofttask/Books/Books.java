bbfb5f671dd1aa0e8a4741d42fd0b6d059f74d91
faulty337 <faulty337@gmail.com>
2023-12-20 00:55:17 +0900

    Feat : add Test Code

--- src/main/java/com/example/rmsofttask/Books/Books.java
+++ src/main/java/com/example/rmsofttask/Books/Books.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 public class Books {
 
     @Id
