f5bd4c35d1576a4594f7d4226696e065f9b09e9d
faulty337 <faulty337@gmail.com>
2023-12-21 21:10:32 +0900

    Feat : add Response valid

--- src/main/java/com/example/rmsofttask/Users/dto/UserIdReqDto.java
+++ src/main/java/com/example/rmsofttask/Users/dto/UserIdReqDto.java
@@ -1,10 +1,12 @@
 package com.example.rmsofttask.Users.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UserIdReqDto {
+    @NotBlank(message = "유저ID가 비어있습니다.")
     private String userId;
 }
